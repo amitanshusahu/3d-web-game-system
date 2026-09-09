c7c4b999a8d71959882b5846460392a20b41c06c
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-09-09 22:25:34 +0530

    tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,20 @@ export default function App() {
         }}
       >
         <axesHelper />
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        {/* <ambientLight intensity={Math.PI / 2} /> */}
+        {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
+        {/* <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
+        <directionalLight position={[10, 10, 5]} intensity={1} />
         <Physics gravity={[0, -9.81, 0]} debug>
-          <RigidBody colliders="hull" position={[0, 5, 0]}>
-            {/* <Model
+          {/* <RigidBody colliders="hull" position={[0, 0, 0]} type='fixed'>
+            <Model
               modelPath="/models/cyberpunk_hovercar.glb"
               position={[0, 0, 0]}
               rotation={[0, 0, 0]}
               scale={1}
-            /> */}
-            <HoverCar />
-          </RigidBody>
+            />
+          </RigidBody> */}
+          <HoverCar />
           <Player />
           <RigidBody type="fixed" colliders="cuboid">
             <mesh position={[0, -0.05, 0]}>
