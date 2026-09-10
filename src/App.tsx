331864e3ce82455209e78d9c9dc8d4f4213e494d
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-09-10 13:04:55 +0530

    added light from eccrl example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Canvas } from '@react-three/fiber'
 import { Physics, RigidBody } from '@react-three/rapier'
-// import Model from './components/Rendering/models/Model'
+import Model from './components/Rendering/models/Model'
 import Player from './components/GameSystem/Player'
 import PlayerHud from './components/GameSystem/PlayerHud'
-import { HoverCar } from './components/Rendering/models/HoverCar'
+// import { HoverCar } from './components/Rendering/models/HoverCar'
+import * as THREE from 'three'
+import Lights from './components/Rendering/Lights'
 
 export default function App() {
   return (
     <>
       <Canvas
+        shadows={{ type: THREE.PCFShadowMap }}
         camera={{
           fov: 75,
           near: 0.1,
@@ -17,20 +20,17 @@ export default function App() {
         }}
       >
         <axesHelper />
-        {/* <ambientLight intensity={Math.PI / 2} /> */}
-        {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
-        {/* <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
-        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <Lights />
         <Physics gravity={[0, -9.81, 0]} debug>
-          {/* <RigidBody colliders="hull" position={[0, 0, 0]} type='fixed'>
+          <RigidBody colliders="hull" position={[0, 0, 0]} type='fixed'>
             <Model
               modelPath="/models/cyberpunk_hovercar.glb"
               position={[0, 0, 0]}
               rotation={[0, 0, 0]}
               scale={1}
             />
-          </RigidBody> */}
-          <HoverCar />
+          </RigidBody>
+          {/* <HoverCar /> */}
           <Player />
           <RigidBody type="fixed" colliders="cuboid">
             <mesh position={[0, -0.05, 0]}>
