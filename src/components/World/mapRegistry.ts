5b64aad642be8c77de56b87d52ccb0d7ee8bc2fa
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-09-11 19:06:23 +0530

    testing new map

--- src/components/World/mapRegistry.ts
+++ src/components/World/mapRegistry.ts
@@ -1,6 +1,7 @@
 import type { ComponentType } from 'react'
 import { TestMap, TestMapSpawnZones } from '../Rendering/map/TestMap'
 import type { SpawnZone } from './worldTypes'
+import { StrongHoldAnimated, StrongHoldSpawnZones } from '../Rendering/map/StrongHoldAnimated'
 
 export interface MapEntry {
   component: ComponentType
@@ -10,4 +11,5 @@ export interface MapEntry {
 /** Global key-value registry: map id -> map component + its spawn zones */
 export const MAP_REGISTRY: Record<string, MapEntry> = {
   testMap: { component: TestMap, spawnZones: TestMapSpawnZones },
+  strongHold: { component: StrongHoldAnimated, spawnZones: StrongHoldSpawnZones },
 }
