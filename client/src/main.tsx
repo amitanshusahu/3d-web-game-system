0c8945c6ae33731df61d1f4fde0d71e43c9f02a6
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-09-17 10:14:26 +0530

    setup react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createRouter({
   routeTree,
@@ -19,5 +22,9 @@ const rootElement = document.getElementById('root')!
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
