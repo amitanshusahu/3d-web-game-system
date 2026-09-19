9f0b9444fb96e1d483bf501972db87cea6417a75
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-09-19 21:49:07 +0530

    registry for different assets

---
+++ scripts/registry/maps.ts
@@ -0,0 +1,17 @@
+export const maps = {
+  environments: [
+    '/models/ignore/map/backrooms_vr.glb',
+    '/models/ignore/map/cloud_station.glb',
+    '/models/ignore/map/enchanted_forest_environment.glb',
+    '/models/ignore/map/mobile_home.glb',
+    '/models/ignore/map/robin_hood_in_sherwood_forest.glb',
+    '/models/ignore/map/the_last_stronghold_animated_floating.glb',
+  ],
+  tracks: [
+    '/models/ignore/bazaar_track.glb',
+    '/models/ignore/fast_racing_3d_-_night_city.glb',
+  ],
+  city: [
+    '/models/ignore/neo-tokyo.glb',
+  ],
+}
